8f2c9fe749b1dc2dfdb191eca048f89f125f1280
Dagogo-Clinton-Uranta <dagogouranta@gmail.com>
2025-08-17 11:37:42 +0100

    upgraded react-scripts

--- frontend/src/actions/baseUrl.js
+++ frontend/src/actions/baseUrl.js
@@ -1,7 +1,7 @@
 
  const baseUrl = 'https://bridgeway-account-portal-server.vercel.app'
 
-//const baseUrl = 'http://localhost:5000'
+//const baseUrl = 'http://localhost:5000' 
 
  export default baseUrl
 
